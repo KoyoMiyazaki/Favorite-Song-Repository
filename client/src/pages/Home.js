4efb6dd5219c81ba51dac04e04a7498c3511ae99
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-07-03 01:56:18 +0900

    [CL] add Pagination

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Box, Stack } from "@mui/material";
+import { Box, Pagination, Stack } from "@mui/material";
 import axios from "axios";
 import Title from "../components/Title";
 import SongList from "../components/SongList";
@@ -7,16 +7,23 @@ import RegisterSong from "../components/RegisterSong";
 
 const Home = () => {
   const [songs, setSongs] = useState([]);
+  const [page, setPage] = useState(1);
+  const [numPages, setNumPages] = useState(0);
 
   const getAllSongs = async () => {
-    const res = await axios.get("http://localhost:8000/songs/");
+    const res = await axios.get(`http://localhost:8000/songs?page=${page}`);
     const data = await res.data;
+    setNumPages(parseInt(data.numPages, 10));
     setSongs(data.data.getAllSongs);
   };
 
   useEffect(() => {
     getAllSongs();
-  }, []);
+  }, [page]);
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
 
   return (
     <Stack direction="column" spacing={3}>
@@ -27,6 +34,16 @@ const Home = () => {
       <Box>
         <Title title="Song List" />
         <SongList songs={songs} getAllSongs={() => getAllSongs()} />
+        <Box
+          sx={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}
+        >
+          <Pagination
+            count={numPages}
+            page={page}
+            onChange={handlePageChange}
+            size="large"
+          />
+        </Box>
       </Box>
     </Stack>
   );
